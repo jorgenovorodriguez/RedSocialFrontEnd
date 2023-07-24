e5205cdf66620178e71e8bc1e57742a5cb9d61f2
carlosanchez <caarlosanchezbjj@gmail.com>
2023-07-25 00:04:10 +0200

    modificacion realizada

--- src/components/Publication/Publication.jsx
+++ src/components/Publication/Publication.jsx
@@ -1,45 +1,47 @@
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 import PublicationHeader from './PublicationHeader/PublicationHeader';
 import PublicationBody from './PublicationBody/PublicationBody';
 import PublicationFooter from './PublicationFooter/PublicationFooter';
 
 const Publication = ({
-    publication,
-    toogleLike,
-    deletePublication,
-    loading,
+  publication,
+  toogleLike,
+  deletePublication,
+  loading,
 }) => {
-    return (
-        <li>
-            <PublicationHeader
-                username={publication.author}
-                createdAt={publication.createdAt}
-                place={publication.place}
-            />
-            <PublicationBody
-                text={publication.text}
-                photoName={publication.photoName}
-                description={publication.description}
-            />
-            <PublicationFooter
-                publicationId={publication.id}
-                owner={publication.owner}
-                likes={publication.likes}
-                likedByMe={publication.likedByMe}
-                toogleLike={toogleLike}
-                deletePublication={deletePublication}
-                loading={loading}
-            />
-        </li>
-    );
+  return (
+    <li>
+      <PublicationHeader
+        username={publication.author}
+        createdAt={publication.createdAt}
+        place={publication.place}
+      />
+      <Link to={`/singlepublication/${publication.id}`}>
+        <PublicationBody
+          text={publication.text}
+          photoName={publication.photoName}
+          description={publication.description}
+        />
+      </Link>
+      <PublicationFooter
+        publicationId={publication.id}
+        owner={publication.owner}
+        likes={publication.likes}
+        likedByMe={publication.likedByMe}
+        toogleLike={toogleLike}
+        deletePublication={deletePublication}
+        loading={loading}
+      />
+    </li>
+  );
 };
 
 Publication.propTypes = {
-    publication: PropTypes.object,
-    toogleLike: PropTypes.func,
-    deletePublication: PropTypes.func,
-    loading: PropTypes.bool,
+  publication: PropTypes.object,
+  toogleLike: PropTypes.func,
+  deletePublication: PropTypes.func,
+  loading: PropTypes.bool,
 };
 
 export default Publication;
--- src/pages/SinglePublicationPage/SinglePublicationPage.jsx
+++ src/pages/SinglePublicationPage/SinglePublicationPage.jsx
@@ -1,32 +1,40 @@
+import { useParams } from 'react-router-dom';
 import Publication from '../../components/Publication/Publication';
 import usePublications from '../../hooks/usePublications';
-
+import singlePublicationService from '../../services/singlePublicationService';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+import { useEffect, useState } from 'react';
 
 const SinglePublicationPage = () => {
-  const { publications, toogleLike, deletePublication, errMsg, loading } =
-    usePublications();
+  const { toogleLike, deletePublication, errMsg, loading } = usePublications();
+
+  const [publication, setPublication] = useState({});
+
+  const { id } = useParams();
+
+  const fetchPublication = async (id) => {
+    const publi = await singlePublicationService(id);
+
+    console.log(publi);
+    setPublication(publi);
+  };
+
+  useEffect(() => {
+    fetchPublication(id);
+  }, []);
 
   return (
     <main>
       {loading && <p>Loading...</p>}
 
       {errMsg && <ErrorMessage msg={errMsg} />}
-      {publications.length > 0 ? (
-        publications.map((publication) => {
-          return (
-            <Publication
-              key={publication.id}
-              publication={publication}
-              toogleLike={toogleLike}
-              deletePublication={deletePublication}
-              loading={loading}
-            />
-          );
-        })
-      ) : (
-        <li>No hay publicaciones</li>
-      )}
+      <Publication
+        key={publication.id}
+        publication={publication}
+        toogleLike={toogleLike}
+        deletePublication={deletePublication}
+        loading={loading}
+      />
     </main>
   );
 };
