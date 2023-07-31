3f1850b882ffe50f1ff64638e09a495c3cc33925
HACKER <jorge.novo.rodriguez.93@gmail.com>
2023-07-31 21:54:28 +0200

    Navbar completo a falta de detalles esteticos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,37 +4,91 @@ import './Header.css';
 import Logo from '../Logo/Logo';
 
 const Header = () => {
-    const { token, logout, user } = useAuth();
+    const { token, logout } = useAuth();
 
     return (
-        <header className='header-layout'>
-            <NavLink to='/home'>
-                <div className='logo-navbar'>
-                    <Logo />{' '}
+        <header>
+            <nav className='navbar navbar-expand-lg bg-dark '>
+                <div className='container-fluid'>
+                    <NavLink to='/home'>
+                        <div className='logo-navbar'>
+                            <Logo />{' '}
+                        </div>
+                    </NavLink>
+
+                    <button
+                        className='navbar-toggler btn btn-light bg-light '
+                        type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarSupportedContent'
+                        aria-controls='navbarSupportedContent'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                    >
+                        <span className='navbar-toggler-icon '></span>
+                    </button>
+                    <div
+                        className='collapse navbar-collapse'
+                        id='navbarSupportedContent'
+                    >
+                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                            {!token && (
+                                <>
+                                    <li className='nav-item '>
+                                        <a
+                                            className='nav-link active text-white'
+                                            aria-current='page'
+                                            href='/login'
+                                        >
+                                            Login
+                                        </a>
+                                    </li>
+                                    <li className='nav-item'>
+                                        <a
+                                            className='nav-link active text-white'
+                                            aria-current='page'
+                                            href='/register'
+                                        >
+                                            Registro
+                                        </a>
+                                    </li>
+                                </>
+                            )}
+
+                            <li className='nav-item'>
+                                <a
+                                    className='nav-link active text-white'
+                                    aria-current='page'
+                                    href='/users'
+                                >
+                                    Usuarios
+                                </a>
+                            </li>
+
+                            {token && (
+                                <>
+                                    <li className='nav-item'>
+                                        <a
+                                            className='nav-link active text-white'
+                                            aria-current='page'
+                                            href='/message'
+                                        >
+                                            Nueva Publicación
+                                        </a>
+                                    </li>
+                                    <li
+                                        className='nav-item'
+                                        onClick={() => logout()}
+                                    >
+                                        <a className='nav-link active text-white'>
+                                            Cerrar Sesión
+                                        </a>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
                 </div>
-            </NavLink>
-            <nav>
-                {user && <p>@{user.username}</p>}
-                {!token && (
-                    <>
-                        <div>
-                            <NavLink to='/login'>Login</NavLink>
-                        </div>
-                        <div>
-                            <NavLink to='/register'>Registro</NavLink>
-                        </div>
-                    </>
-                )}
-                {token && (
-                    <>
-                        <div>
-                            <NavLink to='/message'>Mensaje</NavLink>
-                        </div>
-                        <div onClick={() => logout()}>
-                            <button>Cerrar cesión</button>
-                        </div>
-                    </>
-                )}
             </nav>
         </header>
     );
